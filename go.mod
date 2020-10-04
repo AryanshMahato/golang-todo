8cfa6b705f6b524f46ba4cbfa3e365e61260114c
AryanshMahato <aryanshmahato@gmail.com>
2020-10-04 22:43:33 +0530

    Database connection added

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module GoLang
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/lib/pq v1.8.0
+)
--- main.go
+++ main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"GoLang/controllers"
+	"GoLang/model"
+	"database/sql"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
 	"log"
+	"os"
 )
 
 func main() {
 	server := gin.Default()
+
+	connectDatabase()
 
 	server.GET("/health", controllers.HealthController)
 
@@ -15,4 +22,26 @@ func main() {
 	if err != nil {
 		log.Fatal("Cannot start server", err)
 	}
+}
+
+func connectDatabase() {
+	var (
+		user     = os.Getenv("user")
+		dbname   = os.Getenv("dbname")
+		password = os.Getenv("password")
+	)
+
+	connStr := fmt.Sprintf("user=%v dbname=%v password=%v sslmode=disabled",
+		user,
+		dbname,
+		password,
+	)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		fmt.Println("Database Connection failed", err)
+		return
+	}
+
+	model.DB = db
 }
